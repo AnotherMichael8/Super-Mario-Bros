1931d1b43361b4cb143338e3c1a2d23d8836d03a
AnotherMichael8 <130487522+AnotherMichael8@users.noreply.github.com>
2024-01-15 23:50:39 -0500

    Working on sprites

--- SuperMarioBros/SuperMarioBros/PlayerCharacter/Interfaces/IPlayerSprite.cs
+++ SuperMarioBros/SuperMarioBros/PlayerCharacter/Interfaces/IPlayerSprite.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace SuperMarioBros.PlayerCharacter.Interfaces
 {
     public interface IPlayerSprite
     {
+        public void Update();
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, Color color);
     }
 }
--- SuperMarioBros/SuperMarioBros/PlayerCharacter/PlayerSprites/LeftMovingPlayerSprite.cs
+++ SuperMarioBros/SuperMarioBros/PlayerCharacter/PlayerSprites/LeftMovingPlayerSprite.cs
@@ -1,4 +1,6 @@
-﻿using SuperMarioBros.PlayerCharacter.Interfaces;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SuperMarioBros.PlayerCharacter.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,24 @@ namespace SuperMarioBros.PlayerCharacter.PlayerSprites
 {
     public class LeftMovingPlayerSprite : IPlayerSprite
     {
+        private Texture2D texture;
+        private readonly Rectangle sourceRectangle = new Rectangle();
+        private Rectangle destinationRectangle;
+        private int frameCounter;
+        public LeftMovingPlayerSprite(Texture2D texture)
+        {
+            this.texture = texture;
+            frameCounter = 0;
+        }
+
+        public void Update()
+        {
+            frameCounter++;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, Color color)
+        {
+            destinationRectangle = new Rectangle((int)position.X, (int)position.Y, sourceRectangle.Width * 3, sourceRectangle.Height * 3);
+        }
     }
 }
